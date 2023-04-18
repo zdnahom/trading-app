9058c25a2c4c972a7900ac1708dfa331668f1cce
Nahom <zerihunnahom@gmail.com>
2023-04-18 16:29:26 +0300

    Render the currency pairs to the screen

--- src/components/CurrencyPage.js
+++ src/components/CurrencyPage.js
@@ -1,18 +1,29 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getExchangeData } from '../redux/features/exchange/exchangeSlice';
+import CurrencyPair from './CurrencyPair';
 
 const CurrencyPage = () => {
-  const { exchangeData } = useSelector((store) => store.exchange);
+  const { exchangeData, isLoading, error } = useSelector((store) => store.exchange);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getExchangeData());
   }, [dispatch]);
-
-
+  console.log(exchangeData);
   return (
     <section>
       Currency exchange page
+      {error && <div>Failed to fetch the data</div>}
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <ul>
+          {
+            exchangeData.map((item) => <CurrencyPair key={item.ticker} exchangeItem={item} />)
+          }
+        </ul>
+      )}
+
     </section>
   );
 };
--- src/components/CurrencyPair.js
+++ src/components/CurrencyPair.js
@@ -1,4 +1,41 @@
-const CurrencyPair = () => (
-  <h3>currency pair</h3>
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faClock } from '@fortawesome/free-solid-svg-icons';
+
+const CurrencyPair = ({ exchangeItem }) => (
+  <li>
+    <FontAwesomeIcon icon={faArrowRight} />
+    <div>
+      <div>
+        {'Bid : '}
+        <span>{exchangeItem.bid}</span>
+        {'Ask : '}
+        <span>{exchangeItem.ask}</span>
+      </div>
+      <div>
+        <h3>{exchangeItem.ticker}</h3>
+        <span>
+          <FontAwesomeIcon icon={faClock} />
+          {exchangeItem.date}
+          {' '}
+          | Currency
+        </span>
+      </div>
+    </div>
+
+  </li>
 );
+
 export default CurrencyPair;
+CurrencyPair.propTypes = {
+  exchangeItem: PropTypes.shape({
+    ticker: PropTypes.string.isRequired,
+    bid: PropTypes.string.isRequired,
+    ask: PropTypes.string.isRequired,
+    open: PropTypes.string.isRequired,
+    low: PropTypes.string.isRequired,
+    high: PropTypes.string.isRequired,
+    changes: PropTypes.number.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+};
