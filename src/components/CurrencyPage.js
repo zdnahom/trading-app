5bccdca6176297d658d4d793e376cbcc3c784eae
Nahom <zerihunnahom@gmail.com>
2023-04-18 18:46:34 +0300

    Modify page

--- src/components/CurrencyPage.js
+++ src/components/CurrencyPage.js
@@ -1,23 +1,28 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getExchangeData } from '../redux/features/exchange/exchangeSlice';
+// import { getExchangeData } from '../redux/features/exchange/exchangeSlice';
 import CurrencyPair from './CurrencyPair';
+import '../styles/CurrencyPage.css';
 
 const CurrencyPage = () => {
   const { exchangeData, isLoading, error } = useSelector((store) => store.exchange);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getExchangeData());
+    // dispatch(getExchangeData());
   }, [dispatch]);
   console.log(exchangeData);
   return (
     <section>
-      Currency exchange page
+      <div className="upper-part">
+        <span>Total Currency pairs </span>
+        <span className="total-currency-pairs">{exchangeData.length}</span>
+      </div>
+      <h1 className="currency-page-header">Currency exchange</h1>
       {error && <div>Failed to fetch the data</div>}
       {isLoading ? (
         <div className="loading">Loading...</div>
       ) : (
-        <ul>
+        <ul className="currency-container">
           {
             exchangeData.map((item) => <CurrencyPair key={item.ticker} exchangeItem={item} />)
           }
