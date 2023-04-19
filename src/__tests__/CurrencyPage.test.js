a525fc960c9070f0731dd700f544c8ed3f2ee515
Nahom <zerihunnahom@gmail.com>
2023-04-19 03:09:57 +0300

    Fix linters

--- src/__tests__/CurrencyPage.test.js
+++ src/__tests__/CurrencyPage.test.js
@@ -1,53 +1,53 @@
-import { render, cleanup } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
-import "@testing-library/jest-dom/extend-expect";
-import { configureStore } from "@reduxjs/toolkit";
-import CurrencyPage from "../components/CurrencyPage";
+import { render, cleanup } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import { configureStore } from '@reduxjs/toolkit';
+import CurrencyPage from '../components/CurrencyPage';
 
-afterEach(cleanup)
+afterEach(cleanup);
 const initialState = {
-    exchangeData: [
-      {
-        ticker: "EUR/USD",
-        bid: "1.09734",
-        ask: "1.09734",
-        open: "1.09720",
-        low: "1.09680",
-        high: "1.09755",
-        changes: 0.000140000000000029,
-        date: "2023-04-18 18:48:20",
-      },
-      {
-        ticker: "USD/JPY",
-        bid: "134.060",
-        ask: "134.060",
-        open: "134.122",
-        low: "133.984",
-        high: "134.138",
-        changes: -0.06200000000001182,
-        date: "2023-04-18 18:48:20",
-      },
-    ],
-    clickedPair: [],
-    isLoading: false,
-    error: null,
-  };
-  const store = configureStore({
-    reducer: {
-      exchange: (state = initialState) => state,
+  exchangeData: [
+    {
+      ticker: 'EUR/USD',
+      bid: '1.09734',
+      ask: '1.09734',
+      open: '1.09720',
+      low: '1.09680',
+      high: '1.09755',
+      changes: 0.000140000000000029,
+      date: '2023-04-18 18:48:20',
     },
-  });
+    {
+      ticker: 'USD/JPY',
+      bid: '134.060',
+      ask: '134.060',
+      open: '134.122',
+      low: '133.984',
+      high: '134.138',
+      changes: -0.06200000000001182,
+      date: '2023-04-18 18:48:20',
+    },
+  ],
+  clickedPair: [],
+  isLoading: false,
+  error: null,
+};
+const store = configureStore({
+  reducer: {
+    exchange: (state = initialState) => state,
+  },
+});
 
-describe("CurrencyPage component test", () => {
-  it("Render Currency pair lists", () => {
+describe('CurrencyPage component test', () => {
+  it('Render Currency pair lists', () => {
     const { container } = render(
       <BrowserRouter>
         <Provider store={store}>
           <CurrencyPage />
         </Provider>
-      </BrowserRouter>
+      </BrowserRouter>,
     );
     expect(container).toMatchSnapshot();
   });
